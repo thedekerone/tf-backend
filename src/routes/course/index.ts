0347f5b5455efa1cfef84fcd13ce1b16867853a0
Mauricio Fow <mauricio.fow@quanturi.com>
2024-11-05 18:59:17 +0200

    added find users by group

--- src/routes/course/index.ts
+++ src/routes/course/index.ts
@@ -1,70 +1,116 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { AuthenticatedRequest, authenticateToken, getUserId } from '../../middleware/authMiddleware';
+import {
+	AuthenticatedRequest,
+	authenticateToken,
+	getUserId,
+} from "../../middleware/authMiddleware";
 
-import prisma from '../../prismaClient';
-import { JwtPayload } from 'jsonwebtoken';
+import prisma from "../../prismaClient";
+import { JwtPayload } from "jsonwebtoken";
 
 const router = Router();
 
-router.get('/list-courses', authenticateToken, async (_: AuthenticatedRequest, res) => {
-	try {
-		const courses = await prisma.course.findMany()
+router.get(
+	"/list-courses",
+	authenticateToken,
+	async (_: AuthenticatedRequest, res) => {
+		try {
+			const courses = await prisma.course.findMany();
 
-		res.json(courses);
-	} catch (e) {
-		res.status(500).json({ message: "Unexpected error" });
-	}
-});
+			res.json(courses);
+		} catch (e) {
+			res.status(500).json({ message: "Unexpected error" });
+		}
+	},
+);
 
+router.get(
+	"/my-courses",
+	authenticateToken,
+	async (req: AuthenticatedRequest, res) => {
+		try {
+			const userId = getUserId(req.user as JwtPayload);
 
-router.get('/my-courses', authenticateToken, async (req: AuthenticatedRequest, res) => {
-	try {
-		const userId = getUserId(req.user as JwtPayload);
-
-		const courses = await prisma.course.findMany({
-			where: {
-				users: {
-					some: {
-						id: userId,
+			const courses = await prisma.course.findMany({
+				where: {
+					users: {
+						some: {
+							id: userId,
+						},
 					},
 				},
-			},
-		});
+			});
 
-		res.json(courses);
-	}
-	catch (e) {
-		console.log(e);
-		res.status(500).json({ message: "Unexpected error" });
-	}
-});
-
-router.get('/get-course/:id', authenticateToken, async (req: AuthenticatedRequest, res) => {
-	try {
-		const courseId = req.params.id
-		if (!courseId) {
-			res.status(400).json({ message: "Invalid course ID" });
-			return;
+			res.json(courses);
+		} catch (e) {
+			console.log(e);
+			res.status(500).json({ message: "Unexpected error" });
 		}
+	},
+);
 
-		const course = await prisma.course.findUnique({
-			where: { id: courseId },
-		});
+router.get(
+	"/get-course/:id",
+	authenticateToken,
+	async (req: AuthenticatedRequest, res) => {
+		try {
+			const courseId = req.params.id;
+			if (!courseId) {
+				res.status(400).json({
+					message: "Invalid course ID",
+				});
+				return;
+			}
 
-		if (!course) {
-			res.status(404).json({ message: "Course not found" });
-			return;
+			const course = await prisma.course.findUnique({
+				where: { id: courseId },
+			});
+
+			if (!course) {
+				res.status(404).json({
+					message: "Course not found",
+				});
+				return;
+			}
+
+			res.json(course);
+		} catch (e) {
+			console.log(e);
+			res.status(500).json({ message: "Unexpected error" });
 		}
+	},
+);
 
-		res.json(course);
-	} catch (e) {
-		console.log(e);
-		res.status(500).json({ message: "Unexpected error" });
-	}
-});
+router.get(
+	"/get-users-by-course/:id",
+	authenticateToken,
+	async (req: AuthenticatedRequest, res) => {
+		try {
+			const courseId = req.params.id;
+			if (!courseId) {
+				res.status(400).json({
+					message: "Invalid course ID",
+				});
+				return;
+			}
 
+			const users = await prisma.user.findMany({
+				where: {
+					courses: {
+						some: {
+							id: courseId,
+						},
+					},
+				},
+			});
 
-
+			res.json(users);
+		} catch (e) {
+			console.log(e);
+			res.status(500).json({ message: "Unexpected error" });
+		}
+	},
+);
 
 export const courseRouter = router;
